d41dc59424b6212e859f2fd363b9915a01454a2e
ChethanTN7 <chethantn947@gmail.com>
2024-08-14 22:01:48 +0530

    added the modals

---
+++ index.js
@@ -0,0 +1,13 @@
+const state = {
+    taskList:[]
+}
+
+const taskContent = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+
+console.log(taskContent);
+console.log(taskModal);
+
+const htmlTaskContents = ({id, title, type, description, url}) =>{
+
+}
